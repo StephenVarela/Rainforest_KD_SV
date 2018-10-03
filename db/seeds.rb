8dbc4f7593b963095c5fb4fc779e3dd1e2d63185
StephenVarela <varelast92@gmail.com>
2018-10-03 11:00:37 -0400

    added seed file for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gi =  Product.new(name: 'BJJ Gi',
+             description: 'Marital art gi for competition',
+             price_in_cents: 170000
+             )
+
+
+
+comic =  Product.new(name: 'Comic Book',
+                      description: 'Short Graphic Novel',
+                      price_in_cents: 4000
+                      )
+
+
+sousvide = Product.new(name: 'Sous Vide',
+                        description: 'Used to make really good steak ',
+                        price_in_cents: 100000)
+
+gi.save
+comic.save
+sousvide.save
