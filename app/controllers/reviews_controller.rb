2b57bbe488de6c5493dc8a4a41c0369a22b3c208
StephenVarela <varelast92@gmail.com>
2018-10-03 16:45:00 -0400

    Added review submission form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,18 @@
 class ReviewsController < ApplicationController
   def create
+    #review submit calls this method
+    @review = Review.new(comment: params[:review][:comment], product_id: params[:product_id])
+    if @review.save
+      redirect_to product_url(@review.product)
+    else
+      render '/products/show'
+    end
   end
 
   def edit
   end
 
-  def update 
+  def update
   end
 
   def destroy
